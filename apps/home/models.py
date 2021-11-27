6364d5adfe2c26e67415b3b9fe19ce2911cfaa61
Kfir German <75075971+KfirGerman@users.noreply.github.com>
2021-11-27 22:38:34 +0200

    edit temp

--- apps/home/models.py
+++ apps/home/models.py
@@ -6,28 +6,46 @@ Copyright (c) 2019 - present AppSeed.us
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
-class Temp(models.Model):
-    name = models.CharField(max_length=200)
-    header = models.TextField(null=True, blank=True)
-    body = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
 
 class AboutUs(models.Model):
     name = models.CharField(max_length=200)
-    header = models.TextField(null=True, blank=True)
-    body = models.TextField(null=True, blank=True)
+    sub_subject = models.TextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+
 class FAQ(models.Model):
+    subject = models.CharField(max_length=200)
+    sub_subject = models.TextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class Temp(models.Model):
     name = models.CharField(max_length=200)
-    header = models.TextField(null=True, blank=True)
-    body = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TempGeneral(models.Model):
+    name = models.CharField(max_length=200)
+    subject = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TempTravel(models.Model):
+    name = models.CharField(max_length=200)
+    subject = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
