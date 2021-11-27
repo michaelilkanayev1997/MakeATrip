b0cef3d471a298ce1de17a10eb50cc3ebd9b96f0
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2021-11-27 15:32:06 +0200

    a

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,9 +8,31 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+<<<<<<< Updated upstream
 class Bla(models.Model):
     name = models.CharField(max_length=200)
     header = models.TextField(null=True, blank=True)
+=======
+class Temp(models.Model):
+    name = models.CharField(max_length=200)
+    header = models.TextField(null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class AboutUs(models.Model):
+    name = models.CharField(max_length=200)
+    header = models.TextField(null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class FAQ(models.Model):
+    subject = models.CharField(max_length=200)
+    sub_subject = models.TextField(null=True, blank=True)
+>>>>>>> Stashed changes
     body = models.TextField(null=True, blank=True)
 
     def __str__(self):
