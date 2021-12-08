24557379a6de641ef3f9ad0335173327bd4ec9ec
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2021-12-08 14:16:32 +0200

    edit terms-of-use

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('temp/', views.temp, name='temp'),
     path('faq/', views.faq, name="faq"),
     path('about-us/', views.about_us, name="about-us"),
+    path('terms-of-use/', views.terms_of_use, name="terms-of-use"),
     path('contact-us/', views.contact_us, name="contact-us"),
     re_path(r'^.*\.*', views.pages, name='pages'),
 
