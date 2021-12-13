be3b227bf6a4f966af4f4e93092f467c0912e6bb
Kfir German <75075971+KfirGerman@users.noreply.github.com>
2021-12-13 02:27:58 +0200

    edit ItineraryCategory Model

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,11 +1,7 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
 from datetime import datetime
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 import uuid
 
 
@@ -73,6 +69,7 @@ class Temp(models.Model):
 ##########################################################################################################
 
 travel_user = get_user_model()
+t_user = User
 
 
 class ItineraryCategory(models.Model):
@@ -86,13 +83,14 @@ class ItineraryCategory(models.Model):
 
 
 class ItineraryPlanner(models.Model):
-    user = models.ForeignKey(travel_user, on_delete=models.DO_NOTHING, blank=True, null=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    user = models.ForeignKey(t_user, on_delete=models.DO_NOTHING, blank=True, null=True)
     destination = models.CharField(max_length=254)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
     travelers = models.IntegerField(null=False, blank=True)
     category = models.ForeignKey(ItineraryCategory, on_delete=models.DO_NOTHING, blank=True, null=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
 
     def __str__(self):
         return "%s -- %s > %s by %s" % (self.start_date, self.end_date, self.destination, self.user)
