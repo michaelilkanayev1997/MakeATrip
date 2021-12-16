b48afcf105f7c8af3e8a02fe37b3baf3e218f213
vovadavidzon <vovadavidzon30@gmail.com>
2021-12-16 15:46:42 +0200

    update models

--- apps/home/migrations/0027_edit_ContactUs.py
+++ apps/home/migrations/0027_edit_ContactUs.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0025_edit_ContactUs'),
+        ('home', '0026_add_review'),
     ]
 
     operations = [
