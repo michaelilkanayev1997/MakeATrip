03d7f947d205b62ff10c6c9ba8cfc8b0390c39b7
vovadavidzon <vovadavidzon30@gmail.com>
2021-12-19 23:07:47 +0200

    edit review

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -30,8 +30,8 @@ urlpatterns = [
     #path('usage/', views.usage, name="usage"),
     path('system-usages/', views.system_usages, name="system-usages"),
     # re_path(r'^.*\.*', views.pages, name='pages'),
-    path('for_usage/', views.usage, name="for_usage"),
-    path('popular/', views.report_most_popular, name="popular"),
+   # path('for_usage/', views.usage, name="for_usage"),
+    #path('popular/', views.report_most_popular, name="popular"),
 
 
 
