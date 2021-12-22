b6a5bcc074567c8729aad06185cc2afa04bbc72a
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2021-12-22 16:01:34 +0200

    fixing count_administrator in main page

--- apps/home/views.py
+++ apps/home/views.py
@@ -47,12 +47,15 @@ def review_project(request):
 
 @csrf_protect
 def complaints(request):
-    # complaint = ContactUs.objects.filter(subject='2')
+    now = datetime.now()
+    earlier = now - timedelta(days=7)
+    maximum_days = now - timedelta(days=1000)
+
     count = ContactUs.objects.filter(subject='2').count()
     count_handled = ContactUs.objects.filter(subject='2', complete='1').count()
     count_unhandled = ContactUs.objects.filter(subject='2', complete='0').count()
     count_administrator = ContactUs.objects.filter(subject='2', complete='0',
-                                                   created_date=datetime.now() - timedelta(days=7)).count()
+                                                   created_date__range=(maximum_days, earlier)).count()
 
     context = {'asd': complaint, 'count': count, 'count_handled': count_handled,
                'count_unhandled': count_unhandled, 'count_administrator': count_administrator}
