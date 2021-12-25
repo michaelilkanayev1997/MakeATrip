7d424286d7a381a0a24bf0704f30ea873a00be29
Kfir German <75075971+KfirGerman@users.noreply.github.com>
2021-12-25 15:58:01 +0200

    update model.py

--- apps/home/models.py
+++ apps/home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
 import uuid
 
 ##########################################################################################################
@@ -113,7 +114,8 @@ class ItineraryCategory(models.Model):
 class ItineraryPlanner(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     user = models.ForeignKey(t_user, on_delete=models.CASCADE, blank=True, null=True)
-    session_id = models.CharField(max_length=32, null=True)
+    session = models.ForeignKey(Session, on_delete=models.SET_NULL, blank=True, null=True)
+    # session_id = models.CharField(max_length=32, null=True)
     destination = models.CharField(max_length=254)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
